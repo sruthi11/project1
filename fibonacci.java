f3fb1be0ed435ca5c539b39798833c18e3c439b3
Manosruthi Macha <manosruthimacha@Manosruthis-MacBook-Pro.local>
2018-02-12 01:55:59 -0600

    java program of fibonacci

---
+++ fibonacci.java
@@ -0,0 +1,55 @@
+{\rtf1\ansi\ansicpg1252\cocoartf1504\cocoasubrtf830
+{\fonttbl\f0\fnil\fcharset0 Verdana;}
+{\colortbl;\red255\green255\blue255;\red10\green82\blue135;\red255\green255\blue255;\red0\green0\blue0;
+\red176\green0\blue4;\red0\green0\blue255;\red15\green114\blue1;}
+{\*\expandedcolortbl;;\cssrgb\c0\c40000\c60000;\cssrgb\c100000\c100000\c100000;\cssrgb\c0\c0\c0;
+\cssrgb\c75294\c0\c0;\cssrgb\c0\c0\c100000;\cssrgb\c0\c50980\c0;}
+{\*\listtable{\list\listtemplateid1\listhybrid{\listlevel\levelnfc0\levelnfcn0\leveljc0\leveljcn0\levelfollow0\levelstartat1\levelspace360\levelindent0{\*\levelmarker \{decimal\}}{\leveltext\leveltemplateid1\'01\'00;}{\levelnumbers\'01;}\fi-360\li720\lin720 }{\listname ;}\listid1}}
+{\*\listoverridetable{\listoverride\listid1\listoverridecount0\ls1}}
+\margl1440\margr1440\vieww10800\viewh8400\viewkind0
+\deftab720
+\pard\tx220\tx720\pardeftab720\li720\fi-720\qj\partightenfactor0
+\ls1\ilvl0
+\f0\b\fs26 \cf2 \cb3 {\listtext	1	}\expnd0\expndtw0\kerning0
+class
+\b0 \cf0 \'a0FibonacciExample1\{\'a0\'a0\cb1 \
+\pard\tx220\tx720\pardeftab720\li720\fi-720\qj\partightenfactor0
+\ls1\ilvl0
+\b \cf2 \cb3 \kerning1\expnd0\expndtw0 {\listtext	2	}\expnd0\expndtw0\kerning0
+public
+\b0 \cf0 \'a0
+\b \cf2 static
+\b0 \cf0 \'a0
+\b \cf2 void
+\b0 \cf0 \'a0main(String\'a0args[])\'a0\'a0\cb1 \
+\pard\tx220\tx720\pardeftab720\li720\fi-720\qj\partightenfactor0
+\ls1\ilvl0\cf0 \cb3 \kerning1\expnd0\expndtw0 {\listtext	3	}\expnd0\expndtw0\kerning0
+\{\'a0\'a0\'a0\'a0\cb1 \
+\ls1\ilvl0\cb3 \kerning1\expnd0\expndtw0 {\listtext	4	}\expnd0\expndtw0\kerning0
+\'a0
+\b \cf2 int
+\b0 \cf0 \'a0n1=\cf5 0\cf0 ,n2=\cf5 1\cf0 ,n3,i,count=\cf5 10\cf0 ;\'a0\'a0\'a0\'a0\cb1 \
+\ls1\ilvl0\cb3 \kerning1\expnd0\expndtw0 {\listtext	5	}\expnd0\expndtw0\kerning0
+\'a0System.out.print(n1+\cf6 "\'a0"\cf0 +n2);\cf7 //printing\'a00\'a0and\'a01\'a0\'a0\cf0 \'a0\'a0\cb1 \
+\ls1\ilvl0\cb3 \kerning1\expnd0\expndtw0 {\listtext	6	}\expnd0\expndtw0\kerning0
+\'a0\'a0\'a0\'a0\cb1 \
+\ls1\ilvl0\cb3 \kerning1\expnd0\expndtw0 {\listtext	7	}\expnd0\expndtw0\kerning0
+\'a0
+\b \cf2 for
+\b0 \cf0 (i=\cf5 2\cf0 ;i<count;++i)\cf7 //loop\'a0starts\'a0from\'a02\'a0because\'a00\'a0and\'a01\'a0are\'a0already\'a0printed\'a0\'a0\cf0 \'a0\'a0\cb1 \
+\ls1\ilvl0\cb3 \kerning1\expnd0\expndtw0 {\listtext	8	}\expnd0\expndtw0\kerning0
+\'a0\{\'a0\'a0\'a0\'a0\cb1 \
+\ls1\ilvl0\cb3 \kerning1\expnd0\expndtw0 {\listtext	9	}\expnd0\expndtw0\kerning0
+\'a0\'a0n3=n1+n2;\'a0\'a0\'a0\'a0\cb1 \
+\ls1\ilvl0\cb3 \kerning1\expnd0\expndtw0 {\listtext	10	}\expnd0\expndtw0\kerning0
+\'a0\'a0System.out.print(\cf6 "\'a0"\cf0 +n3);\'a0\'a0\'a0\'a0\cb1 \
+\ls1\ilvl0\cb3 \kerning1\expnd0\expndtw0 {\listtext	11	}\expnd0\expndtw0\kerning0
+\'a0\'a0n1=n2;\'a0\'a0\'a0\'a0\cb1 \
+\ls1\ilvl0\cb3 \kerning1\expnd0\expndtw0 {\listtext	1	}\expnd0\expndtw0\kerning0
+\'a0\'a0n2=n3;\'a0\'a0\'a0\'a0\cb1 \
+\ls1\ilvl0\cb3 \kerning1\expnd0\expndtw0 {\listtext	2	}\expnd0\expndtw0\kerning0
+\'a0\}\'a0\'a0\'a0\'a0\cb1 \
+\ls1\ilvl0\cb3 \kerning1\expnd0\expndtw0 {\listtext	3	}\expnd0\expndtw0\kerning0
+\'a0\'a0\cb1 \
+\ls1\ilvl0\cb3 \kerning1\expnd0\expndtw0 {\listtext	4	}\expnd0\expndtw0\kerning0
+\}\}\'a0\'a0}
